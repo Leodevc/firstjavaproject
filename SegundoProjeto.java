f17a1358e3a0c2d58e3d2fce59cd2b5990252675
Leonardo Costa <162723638+Leodevc@users.noreply.github.com>
2024-08-12 11:04:47 -0300

    Add files via upload

---
+++ SegundoProjeto.java
@@ -0,0 +1,16 @@
+package br.com.aula01;
+
+public class SegundoProjeto {
+
+	public static void main(String[] args) {
+		
+		int cel = 10;
+		
+		System.out.println ("A temperatura em celsius é: " + cel);
+		
+		double faren = (1.8 * cel) + 32;
+	    
+		System.out.println ("Essa temperatura em farenheit é: " + faren);
+	}
+
+}
